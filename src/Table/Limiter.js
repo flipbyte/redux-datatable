7485b771e425dcac8a9a89f61ae3189301ee80d0
Eon Sequeira <eon@earncef.com>
2018-06-10 17:30:51 +0530

    Css fix

--- src/Table/Limiter.js
+++ src/Table/Limiter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 const Limiter = ({ options, setLimit }) =>
-    <label className="limiter pull-right">
+    <label className="limiter d-flex justify-content-end">
         <select className="form-control input-sm" id="limiter" onChange={ (event) => setLimit(event.target.value) }>
             { options.map( (option, index) =>
                 <option key={ index }>{ option }</option>
