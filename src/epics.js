9ad4a5d902813a84efdab67bfb3047bc77a5e8a3
Eon Sequeira <eon@earncef.com>
2018-10-14 20:09:14 +0530

    Upgraded to rxjs6

--- src/epics.js
+++ src/epics.js
@@ -1,7 +1,7 @@
-import { Observable  } from 'rxjs/Observable';
 import get from 'lodash/get';
-import { concatMap, switchMap, map, takeUntil, filter } from 'rxjs/operator';
-import { of } from 'rxjs/observable/of';
+import { ofType } from 'redux-observable';
+import { Observable, of, pipe } from 'rxjs';
+import { concatMap, switchMap, map, takeUntil, filter } from 'rxjs/operators';
 import {
     actionCreators,
     REQUEST_DATA,
@@ -16,63 +16,65 @@ import {
     DELETE_DATA
 } from './actions';
 
-export const setParamsEpic = ( action$, store ) =>
-    action$.ofType(
-        SET_PAGE,
-        SET_FILTER,
-        SET_SORT,
-        SET_LIMIT,
-    ).concatMap( action => {
-        const { name, routes, reducerName } = action.meta;
+export const setParamsEpic = ( action$, state$ ) => action$.pipe(
+    ofType(SET_PAGE, SET_FILTER, SET_SORT, SET_LIMIT),
+    concatMap(action => {
+        const { meta: { name, routes, reducerName } } = action;
 
-        return Observable.of(
+        return of(
             actionCreators.cancelRequest({ name }),
             actionCreators.requestData({
-                name, routes, reducerName, payload: { query: get(store.getState(), [reducerName, name]).query }
+                name, routes, reducerName, payload: { query: get(state$.value, [reducerName, name]).query }
             })
         )
-    });
+    })
+);
 
-export const fetchDataEpic = ( action$, store, { apiGet, schemas } ) =>
-    action$.ofType(REQUEST_DATA).switchMap( action => {
+export const fetchDataEpic = ( action$, state$, { api }) => action$.pipe(
+    ofType(REQUEST_DATA),
+    switchMap(action => {
         const {
             meta: { name, routes },
             payload
         } = action;
 
-        return apiGet(routes.get.route, Object.assign({}, routes.get.params, payload.query)).execute()
-            .map(response => {
-                const requestResponse = get(response, routes.get.resultPath.response, null);
-                const data = get(response, routes.get.resultPath.data, null);
-                const payload = { response: requestResponse, data };
-
+        return api.get(routes.get.route, { params: Object.assign({}, routes.get.params, payload.query) }).pipe(
+            map(response => {
+                const data = get(response, routes.get.resultPath.data, {});
+                const payload = { response, data };
                 return actionCreators.receiveData({ name, payload })
-            }).takeUntil(
-                action$.ofType(REQUEST_DATA_CANCEL)
-                    .filter(cancelAction => cancelAction.name == name)
-            )
-    });
+            }),
+            takeUntil(action$.pipe(
+                ofType(REQUEST_DATA_CANCEL),
+                filter(cancelAction => cancelAction.name == name)
+            ))
+        )
+    })
+);
 
-export const deleteDataEpic = ( action$, store, { apiDelete, schemas } ) =>
-    action$.ofType(DELETE_DATA).switchMap( action => {
+export const deleteDataEpic = ( action$, state$, { api }) => action$.pipe(
+    ofType(DELETE_DATA),
+    switchMap(action => {
         const {
             meta: { name, routes, reducerName },
             payload
         } = action;
 
-        return apiDelete(routes.delete.route, payload.params).execute().concatMap(response => {
-            const result = get(response, routes.delete.resultPath, null);
-            if(result.success) {
-                return Observable.of(
-                    actionCreators.setMessage({ type: 'success', message: result.result }),
+        return api.delete(routes.delete.route, { params: payload.params }).pipe(
+            concatMap(response => {
+                if(!response.success) {
+                    return of(actionCreators.setMessage({ type: 'danger', message: response.result }))
+                }
+
+                return of(
+                    actionCreators.setMessage({ type: 'success', message: response.result }),
                     actionCreators.cancelRequest({ name }),
                     actionCreators.requestData({
                         name, routes, reducerName,
-                        payload: { query: get(store.getState(), [reducerName, name]).query }
+                        payload: { query: get(state$.value, [reducerName, name]).query }
                     })
                 );
-            }
-
-            return actionCreators.setMessage({ type: 'danger', message: result.result })
-        })
-    });
+            })
+        )
+    })
+)
