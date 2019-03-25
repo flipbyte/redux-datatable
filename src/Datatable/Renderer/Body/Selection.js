de1fa8884178d7422bc36dcb9721a0a96bb212c0
ES <eon@earncef.com>
2019-03-25 11:55:10 +0530

    Fixed row selection

--- src/Datatable/Renderer/Body/Selection.js
+++ src/Datatable/Renderer/Body/Selection.js
@@ -2,59 +2,35 @@ import _ from 'lodash';
 import PropTypes from "prop-types";
 import { connect } from 'react-redux';
 import React, { Component } from 'react';
-import { setSelection } from '../../../actions';
+import { SET_SELECTION } from '../../../actions';
 import { withTableConfig } from '../../../TableProvider';
 import { getParam, getConfigParam, prepareActionPayload } from '../../../utils';
 import { memoizedGetSelection } from '../../../selectors';
 
-const handleSelection = ({ data, colConfig, setSelection }, event ) => {
-    let dataKey = getConfigParam(colConfig.indexField);
-    let param = getParam(colConfig.indexField, data);
-    setSelection(dataKey, param, event.target.checked)
+const handleSelection = ({ data, colConfig, action }, event ) => {
+    let paramKey = getConfigParam(colConfig.indexField);
+    let key = getParam(colConfig.indexField, data);
+    action(SET_SELECTION)({ paramKey, key, value: event.target.checked });
 }
 
-class Selection extends Component {
-    shouldComponentUpdate( nextProps ) {
-        const { selection } = this.props;
-        const { selection: nextSelection } = nextProps;
+const Selection = ( props ) => {
+    const {
+        data,
+        extraData: { selection },
+        colConfig: { indexField }
+    } = props;
+    const dataKey = getConfigParam(indexField);
+    const key = _.get(data, dataKey);
+    const value = _.get(selection, [dataKey, key], false);
 
-        return selection != nextSelection;
-    }
-
-    render() {
-        const { selection, ...rest } = this.props;
-        return (
-            <div className="col-12">
-                <input
-                    type="checkbox"
-                    checked={ selection }
-                    onChange={ handleSelection.bind(this, rest) } />
-            </div>
-        );
-    }
+    return (
+        <div className="col-12">
+            <input
+                type="checkbox"
+                checked={ value }
+                onChange={ handleSelection.bind(this, props) } />
+        </div>
+    );
 }
 
-const mapStateToProps = ( state, {
-    data,
-    config: { reducerName, name },
-    colConfig: { indexField }
-}) => {
-    let dataKey = getConfigParam(indexField);
-    var key = _.get(data, dataKey);
-
-    return {
-        selection: _.get(state, [reducerName, name, 'selection', dataKey, key], false)
-    }
-}
-
-const mapDispatchToProps = ( dispatch, { config } ) => ({
-    setSelection: ( paramKey, key, value ) =>
-        dispatch(setSelection(prepareActionPayload(config)({ paramKey, key, value }))),
-});
-
-export default withTableConfig({
-    name: 'name',
-    reducerName: 'reducerName',
-    routes: 'routes',
-    entity: 'entity'
-})(connect(mapStateToProps, mapDispatchToProps)(Selection));
+export default Selection;
